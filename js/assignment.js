4633747b5516998aa7a97fd085819700fb1066c5
codeL0gic <shannon.neumann@yum.com>
2025-01-17 18:00:26 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,44 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+// Create a function called findSmallestNumber. This function should take in the array, myNumbers. Loop through the array and return the smallest number.
+function findSmallestNumber(myNumbers) {
+  let smallestNumber = myNumbers[0];
+  for (let i = 0; i < myNumbers.length; i++) {
+    if (myNumbers[i] < smallestNumber) {
+      smallestNumber = myNumbers[i];
+    }
+  }
+  return smallestNumber;
+}
+
+// console.log(findSmallestNumber(myNumbers));
+// Create a function called findLargestNumber. This function should take in the array, myNumbers. Loop through the array and return the largest number.
+
+function findLargestNumber(myNumbers) {
+  let largestNumber = myNumbers[0];
+  for (let i = 0; i < myNumbers.length; i++) {
+    if (myNumbers[i] > largestNumber) {
+      largestNumber = myNumbers[i];
+    }
+  }
+  return largestNumber;
+}
+
+// Create a function called findAverage. This function should take in the array, myNumbers. Loop through the array and return the average.
+
+function findAverage(myNumbers) {
+  let total = 0;
+  for (let i = 0; i < myNumbers.length; i++) {
+    total += myNumbers[i];
+  }
+  return total / myNumbers.length;
+}
+
+// Create the remaining functionality to render your answers.
+submissionBtn.addEventListener("click", () => {
+  smallestNumberElement.innerHTML = findSmallestNumber(myNumbers);
+  largestNumberElement.innerHTML = findLargestNumber(myNumbers);
+  averageNumberElement.innerHTML = findAverage(myNumbers);
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+
+// let children = ["jordan", "nolan", "sophie", "carson"];
+// for (let i = 0; i < children.length; i++) {
+//   console.log(children[i]);
+// }
+
+// let OffSpring = ["jordan", "nolan", "sophie", "carson"];
+// let totalOffSpring = 0;
+// for (let i = 0; i < OffSpring.length; i++) {
+//   totalOffSpring++;
+// }
+
+// console.log(totalOffSpring);
+
+// let Score = [10, 20, 30, 40, 50];
+// for (let i = 0; i < Score.length; i++) {
+//   console.log(Score[i]);
+// }
+// let sum = 0;
+// {
+//   sum += Score;
+// }
+// console.log(Score);
+
+// // let Score = [10, 20, 30, 40, 50];
+
+// const numbers = [10, 20, 30, 40, 50];
+// numbers.forEach((number) => {
+//   console.log(number);
+// });
+
+let fruits = ["mango", "pineapple", "kiwi", "pomegrante"];
+// fruits.forEach((fruit) => {
+//   //upercase each fruit
+//   fruit.toUpperCase();
+// });
+// console.log(fruits);
+
+// fruits = fruits.map((fruit) => fruit.toUpperCase());
+
+// console.log(fruits);
